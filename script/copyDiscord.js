65c4154528baddf25fddddd85431f00c4418cbaf
Roman <zeltysevr121@gmail.com>
2026-05-20 22:09:34 +1000

    update

--- script/copyDiscord.js
+++
@@ -1,7 +0,0 @@
-const discrodBtn = document.getElementById('copyBtnDiscord')
-
-discrodBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    navigator.clipboard.writeText('stavarachi_')
-    alert('Дискорд скопирован в буфер обмена.')
-})
